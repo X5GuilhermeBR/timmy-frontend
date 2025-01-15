beae97cd825d47c68a886c8e4988e682fcd45f6c
Guilherme José <gui_guifonseca@hotmail.com>
2025-01-15 14:51:49 -0300

    chore: config ant design library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,81 @@
-function App() {
+import React, { useState } from 'react'
+import './index.css'
+import {
+  HomeOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  ReadOutlined,
+  TagOutlined,
+  TeamOutlined,
+} from '@ant-design/icons'
+import { Button, Layout, Menu, theme } from 'antd'
+
+const { Header, Sider, Content } = Layout
+
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false)
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken()
 
   return (
-    <>
-      <h1>TELA DE LOGIN - TIMMY</h1>
-    </>
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <HomeOutlined />,
+              label: 'Inicio',
+            },
+            {
+              key: '2',
+              icon: <TeamOutlined />,
+              label: 'Membresia',
+            },
+            {
+              key: '3',
+              icon: <ReadOutlined />,
+              label: 'Ensino',
+            },
+            {
+              key: '4',
+              icon: <TagOutlined />,
+              label: 'Eventos',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
   )
 }
 
